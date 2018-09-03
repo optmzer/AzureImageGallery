79ba869c473468ae7ddd4cb9a64c4de0848acaf9
Alexander Frolov <big_air2409@mail.ru>
2018-09-03 13:02:09 +1200

    Added services to the project that will interact with a search box

---
+++ AzureImageGallery.Data/IImage.cs
@@ -0,0 +1,12 @@
+﻿using AzureImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace AzureImageGallery.Data
+{
+    public interface IImage
+    {
+        IEnumerable<GalleryImage> GetAll();
+        IEnumerable<GalleryImage> GetWithTag(string tag);
+        GalleryImage GetById(int id);
+    }
+}
---
+++ AzureImageGallery.Services/ImageService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AzureImageGallery.Data;
+using AzureImageGallery.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureImageGallery.Services
+{
+    /**
+     * This service will interact with a search box on the page.
+     */
+    public class ImageService : IImage
+    {
+        private readonly AzureImageGalleryDbContext _context;
+
+        public ImageService(AzureImageGalleryDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IEnumerable<GalleryImage> GetAll()
+        {
+            return _context.GalleryImages.Include(img => img.Tags);
+        }
+
+        public GalleryImage GetById(int id)
+        {
+            return _context.GalleryImages.Find(id);
+        }
+
+        public IEnumerable<GalleryImage> GetWithTag(string tag)
+        {
+            return GetAll()
+                .Where(image => image.Tags.Any(t => t.Description == tag));
+        }
+    }
+}
