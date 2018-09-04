425efd785b8b52bd41fb084e73e491b08a5f9ab5
Alexander Frolov <big_air2409@mail.ru>
2018-09-04 16:54:40 +1200

    Added Upload Model, View and Controller. Added form. Works OK.

---
+++ AzureImageGallery/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using AzureImageGallery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureImageGallery.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ AzureImageGallery/Models/UploadImageModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AzureImageGallery.Models
+{
+    public class UploadImageModel
+    {
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile ImageUpload { get; set; }
+    }
+}
