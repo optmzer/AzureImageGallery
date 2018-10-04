d73f9272a7c1fd061a2ec318bc51209111ad2b88
Alexander Frolov <big_air2409@mail.ru>
2018-10-04 16:06:35 +1300

    Created read me. Added some comments to Startup.cs

--- AzureImageGallery/Startup.cs
+++ AzureImageGallery/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AzureImageGallery.Data;
+﻿using AzureImageGallery.Data;
 using AzureImageGallery.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -24,7 +19,8 @@ namespace AzureImageGallery
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // References DefaultConnection from appsettings.json
---
+++ README.md
@@ -0,0 +1,37 @@
+# AzureImageGallery
+
+This is a practice project for ASP.NET Core. Creating an MVC web app to upload images to Azure Blob storage.
+
+## Content
+
+Project description:  
+- It allows a user to upload pictures via a web form to Azure blob storage.  
+- The app stores picture entries in the form of a database created with MSSQLlocalDB.
+
+## Folder Structure
+
+```
+```
+
+## Prerequisites
+
+```
+
+```
+  
+>NOTE:  
+>-   
+>- 
+
+
+## Contributing
+
+This is a study project so contributing is not required. Please consider other projects. Thank you for your interest.
+
+## Versioning
+
+For the versions available, see the tags on this repository.
+
+## Authors
+
+Reprint from youtube video by [Wes Doyle](https://www.youtube.com/watch?v=UsT6xvdNMJw)
